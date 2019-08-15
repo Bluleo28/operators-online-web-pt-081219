bd62b7a285b75d29657abfcafa37ae47a9929db4
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-15 02:25:52 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,19 +1,19 @@
-def unsafe?
+def 
   #unsafe=true(60)
 end
-def unsafe?
+def
   #unsafe=true(40)
 end
-def unsafe?
+def 
   #unsafe?=(50)
 end
-def not_safe?
+def 
 	#not_safe?=(70)
 end
-def not_safe?
+def 
   #not_safe?=(30)
 end
-def not_safe?
+def
   #not_safe?=(50)
 end
 
